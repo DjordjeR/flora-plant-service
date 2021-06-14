e1d10907f67eb3152b72ac85de672c977ea8c5cc
toma <ntomic@live.com>
2021-06-14 10:55:18 +0200

    tests

--- main/src/tests/test_main.py
+++ main/src/tests/test_main.py
@@ -25,7 +25,9 @@ def server():
 
 
 ##################
+#####  GET  ######
 ##################
+
 
 # Get available plants
 def test_plant_get():
@@ -33,70 +35,12 @@ def test_plant_get():
     assert response.status_code == 200
 
 
-# Create new plant
-# def test_post_plant_new():
-#     temp_plant_name = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
-
-
-#     data = {'common_name': temp_plant_name, 'latin_name': temp_plant_name}
-#     response = requests.post(base_url + "/plant", json=data)
-
-#     assert response.status_code == 2
-# Try to create a duplicate
-def test_post_plant_duplicate():
-    data = {'common_name': 'gucci', 'latin_name': 'gucci'}
-    response = requests.post(base_url + "/plant", json=data)
-
-    assert response.status_code == 422
-
-
-# Missing common_name parameter
-def test_post_plant_missing_common_name():
-    data = {'latin_name': 'gucci'}
-    response = requests.post(base_url + "/plant", json=data)
-
-    response_json = response.json()
-    assert response.status_code == 422
-    assert response_json['detail'][0]['loc'][1] == 'common_name'
-   
-
-# Missing latin_name parameter
-def test_post_plant_missing_latin_name():
-    data = {'common_name': 'gucci'}
-    response = requests.post(base_url + "/plant", json=data)
-
-    response_json = response.json()
-    assert response.status_code == 422
-    assert response_json['detail'][0]['loc'][1] == 'latin_name'
-
-
-# Missing both parameters
-def test_post_plant_missing_latin_name():
-    data = ' '
-    response = requests.post(base_url + "/plant", json=data)
-
-    response_json = response.json()
-    assert response.status_code == 422
-    assert response_json['detail'][0]['loc'][1] == 'common_name'
-    assert response_json['detail'][1]['loc'][1] == 'latin_name'
-
-
-# Non Json body
-def test_post_plant_non_json_body():
-    response = requests.post(base_url + "/plant", data="brate")
-
-    response_json = response.json()
-    assert response.status_code == 422
-    assert response_json['detail'][0]['type'] == 'value_error.jsondecode'
-
-
 # Get certain plant
-def test_get_plant_certain():
+def test_plant_get_certain():
     temp_plant_name = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
 
     data = {'common_name': temp_plant_name, 'latin_name': temp_plant_name}
     response = requests.post(base_url + "/plant", json=data)
-
     assert response.status_code == 200
 
     response = requests.get(base_url + "/plant/" + temp_plant_name)
@@ -107,10 +51,92 @@ def test_get_plant_certain():
     assert response_json['metadata'] == None
 
 
+
+##################
+#####  POST  #####
+##################
+
+# Try to create a duplicate
+def test_plant_post_duplicate():
+    temp_plant_name = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+
+    data = {'common_name': temp_plant_name, 'latin_name': temp_plant_name}
+    response = requests.post(base_url + "/plant", json=data)
+    print(response.json())
+    assert response.status_code == 200
+
+    data = {'common_name': temp_plant_name, 'latin_name': temp_plant_name}
+    response = requests.post(base_url + "/plant", json=data)
+
+    assert response.status_code == 422
+
+
+# Missing common_name parameter
+def test_plant_post_missing_common_name():
+    data = {'latin_name': 'gucci'}
+    response = requests.post(base_url + "/plant", json=data)
+
+    response_json = response.json()
+    assert response.status_code == 422
+    assert response_json['detail'][0]['loc'][1] == 'common_name'
+   
+
+# Missing latin_name parameter
+def test_plant_post_missing_latin_name():
+    data = {'common_name': 'gucci'}
+    response = requests.post(base_url + "/plant", json=data)
+
+    response_json = response.json()
+    assert response.status_code == 422
+    assert response_json['detail'][0]['loc'][1] == 'latin_name'
+
+
+# Missing both parameters
+def test_plant_post_missing_latin_name():
+    data = ' '
+    response = requests.post(base_url + "/plant", json=data)
+
+    response_json = response.json()
+    assert response.status_code == 422
+    assert response_json['detail'][0]['loc'][1] == 'common_name'
+    assert response_json['detail'][1]['loc'][1] == 'latin_name'
+
+
+# Non Json body
+def test_plant_post_non_json_body():
+    response = requests.post(base_url + "/plant", data="brate")
+
+    response_json = response.json()
+    assert response.status_code == 422
+    assert response_json['detail'][0]['type'] == 'value_error.jsondecode'
+
+
 # Get nonexisting plant
-def test_get_plant_certain():
+def test_plant_get_non_existing():
     response = requests.get(base_url + "/plant/gucciguccilouislousfendifendiprada")
 
     response_json = response.json()
     assert response.status_code == 404
     assert response_json["detail"] == 'Object does not exist'
+
+
+
+##################
+#####  PUT  ######
+##################
+
+# Try to create a duplicate
+def test_put_plant():
+    temp_plant_name = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+
+    data = {'common_name': temp_plant_name, 'latin_name': temp_plant_name}
+    response = requests.post(base_url + "/plant", json=data)
+    assert response.status_code == 200
+
+
+
+
+    data = {'common_name': temp_plant_name, 'latin_name': temp_plant_name}
+    response = requests.post(base_url + "/plant", json=data)
+
+    assert response.status_code == 422
