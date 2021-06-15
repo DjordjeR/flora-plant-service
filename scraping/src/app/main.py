ea10beae6059160ed63c2543a11630208e26b16b
ldzzz <eldinabdic47@gmail.com>
2021-06-15 14:07:59 +0200

    ADD: some error handling

--- scraping/src/app/main.py
+++ scraping/src/app/main.py
@@ -45,7 +45,7 @@ runner = CrawlerProcess(settings={
         'BOT_NAME': 'floraSpider',
         'CONCURRENT_REQUESTS_PER_DOMAIN': 16,
         'COOKIES_ENABLED': False,
-        #'LOG_LEVEL': 'ERROR'
+        'LOG_LEVEL': 'ERROR'
     })
 
 def _execute_spider_in_process(q):
@@ -63,41 +63,47 @@ def _execute_spider_in_process(q):
     q.put(plants)
 
 async def run_spider(job_id, search_query):
-    plants = []
-    q = Queue() # may god help you 
-    p = Process(target=_execute_spider_in_process, args=(q,))
-    p.start()
-    loop = asyncio.get_event_loop()
-    plants =  await loop.run_in_executor(None, q.get)
-    await loop.run_in_executor(None, p.join)
+    try:
+        plants = []
+        q = Queue() # may god help you 
+        p = Process(target=_execute_spider_in_process, args=(q,))
+        p.start()
+        loop = asyncio.get_event_loop()
+        plants =  await loop.run_in_executor(None, q.get)
+        await loop.run_in_executor(None, p.join)
 
-    print('AMOUNT OF FOUND PLANTS: ', len(plants))
-    matched_plants = set()
-    for e in plants:
-        ln = e.get('latin_name', '')
-        del e['latin_name']
-        res, _ = await ScrapedPlant.get_or_create(latin_name=ln, defaults=e) #TODO: why dfq is <b> added randomly????
-        ratios = [fuzz.token_set_ratio(cn, search_query) for cn in res.common_names]
-        ratios.append(fuzz.token_set_ratio(res.latin_name, search_query))
-        max_ratio = max(ratios)
-        if max_ratio >= 80:
-            print('Found fuzzy match of {}%'.format(max_ratio)) #TODO: improve fuzzy matching
-            matched_plants.add(res)
+        print('AMOUNT OF FOUND PLANTS: ', len(plants))
+        matched_plants = set()
+        for e in plants:
+            ln = e.get('latin_name', '')
+            del e['latin_name']
+            res, _ = await ScrapedPlant.get_or_create(latin_name=ln, defaults=e)
+            ratios = [fuzz.token_set_ratio(cn, search_query) for cn in res.common_names]
+            ratios.append(fuzz.token_set_ratio(res.latin_name, search_query))
+            max_ratio = max(ratios)
+            if max_ratio >= 80:
+                print('Found fuzzy match of {}%'.format(max_ratio)) #TODO: improve fuzzy matching
+                matched_plants.add(res)
 
-    # after done with adding plants to scraperDB, lets mark job as done and return
-    print('*'*200)
-    print('Amount of matched plants ', len(matched_plants))
-    print('*'*200)
+        # after done with adding plants to scraperDB, lets mark job as done and return
+        print('*'*200)
+        print('Amount of matched plants ', len(matched_plants))
+        print('*'*200)
 
-    #res_pyd = await ScrapedPlant_Pydantic.from_tortoise_orm(res[0])
-    #TODO: update related job, if no plant found put ERROR, myb raise exception on search for plant_name and do it in exception
-    related_job = await ScrapeJob.get(job_id=job_id)
-    related_job.status = JobTypeEnum.done
-    for e in matched_plants:
-        e_pyd =  await ScrapedPlant_Pydantic.from_tortoise_orm(e) #TODO: remove pk
-        related_job.result.append(e_pyd.dict())
-    await related_job.save()
-    print('Job done!')
+        related_job = await ScrapeJob.get(job_id=job_id)
+        related_job.status = JobTypeEnum.stopped # init as stopped and change to done only if at least one plant was found
+        if len(matched_plants):
+            related_job.status = JobTypeEnum.done
+        for e in matched_plants:
+            e_pyd =  await ScrapedPlant_Pydantic.from_tortoise_orm(e)
+            related_job.result.append(e_pyd.dict())
+        await related_job.save()
+        print('Job done!')
+    except Exception as e:
+        print('Something went wrong: -> {}'.format(e))
+        related_job = await ScrapeJob.get(job_id=job_id)
+        related_job.status = JobTypeEnum.error
+        await related_job.save()
 
 
 @app.post("/scrape", tags=["scraper"], response_model=ScrapeResponse, responses={400: {"detail": "Bad request"}})
@@ -108,10 +114,15 @@ async def scrape(scrape_req: ScrapeRequest, background_tasks: BackgroundTasks):
         #TODO: create new job and background task only if task for same thing is not running already, BASE ON plant_name, somehow
         # if task running and fuzzymatch >=80% then dont create background
         # if task ERROR then return cannot find data
-        new_job, _ = await ScrapeJob.get_or_create(status=JobTypeEnum.running, search_query=scrape_req.search_query)
-        job_id = new_job.job_id
-        print('Latest job ID=', job_id) 
-        background_tasks.add_task(run_spider, job_id=job_id, search_query=scrape_req.search_query) #run scraping in background
+        has_job = await ScrapeJob.filter(status__in=[JobTypeEnum.running, JobTypeEnum.error] , search_query=scrape_req.search_query).first()
+        if not has_job:
+            new_job, _ = await ScrapeJob.get_or_create(status=JobTypeEnum.running, search_query=scrape_req.search_query)
+            job_id = new_job.job_id
+            print('Latest job ID=', job_id)
+            print('New job encountered, creating scraping background task')
+            background_tasks.add_task(run_spider, job_id=job_id, search_query=scrape_req.search_query) #run scraping in background
+        else:
+            job_id = has_job.job_id
         return ScrapeResponse(job_id=job_id)
     except Exception as e:
         print(e)
